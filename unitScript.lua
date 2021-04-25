7f556ca8e30d5cdb685b45c97071beb6e03f703b
jwande18 <jwande18@asu.edu>
2021-04-25 13:27:12 -0700

    implemented function to update and calculate wheel velocities

--- unitScript.lua
+++ unitScript.lua
@@ -13,10 +13,12 @@ function sysCall_init()
    unit_position = {0, 0}                -- unit location (x, y) (m)
    unit_accel    = {0, 0}                -- unit acceleration (a_x, a_y) (m/s^2)
    unit_vel      = {0, 0}                -- unit velocity (v_x, v_y) (m/s)
+   unit_ang_vel  = {0, 0}                -- unit angular velocity (v_x, v_y) (rad/s)
    
 end
 
 function sysCall_actuation()
+   updateUnitWheelVelocities()
    -- put your actuation code here
 end
 
@@ -26,6 +28,58 @@ end
 
 function sysCall_cleanup()
     -- do some clean-up here
+end
+
+-- update functions
+function updateUnitWheelVelocities()
+   -- calculate target pose
+   t_velocity = {
+      unit_vel[1] + unit_accel[1], -- v'_x = v_x + a_x
+      unit_vel[2] + unit_accel[2]  -- v'_y = v_y + a_y
+   }
+
+   phi = math.atan2(t_velocity[2], t_velocity[1]) -- desired angle
+   theta = math.atan2(unit_vel[2], unit_vel[1])   -- current angle
+
+   -- angle wrapping
+   alpha = phi - theta -- angle difference
+   alpha = math.atan2(math.sin(alpha), math.cos(alpha))
+   alpha = ((math.pi + alpha) % 2 * math.pi) - math.pi
+
+   -- determine the sign
+   sign = 1
+   if alpha > 0 then
+      sign = 1
+   else
+      sign = -1
+   end
+
+   -- unit angular velocity (omega)
+   new_ang_vel = {
+      unit_ang_vel[1] + unit_accel[1],
+      unit_ang_vel[2] + unit_accel[2]
+   }
+
+   new_ang_vel_mag = _mag(new_ang_vel)
+   t_velocity_mag      = _mag(t_velocity)
+
+   left_wheel_av = (1 / wheel_radius) * (t_velocity_mag + (axle_length / 2) * new_ang_vel_mag)
+   right_wheel_av = (1 / wheel_radius) * (t_velocity_mag - (axle_length / 2) * new_ang_vel_mag)
+
+   -- set the motor velocities
+   sim.setJointTargetVelocity(
+      sim.getObjectHandle('Pioneer_p3dx_leftMotor'),
+      left_wheel_av
+   )
+   sim.setJointTargetVelocity(
+      sim.getObjectHandle('Pioneer_p3dx_rightMotor'),
+      right_wheel_av
+   )
+end
+
+-- helper/internal functions
+function _mag(v)
+   return math.sqrt((v[1] * v[1]) + (v[2] * v[2]))
 end
 
 -- See the user manual or the available code snippets for additional callback functions and details
