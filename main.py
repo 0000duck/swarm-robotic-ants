b99b650675b3928ae9fc67a5ec5708db3367c843
jwande18 <jwande18@asu.edu>
2021-04-25 12:07:26 -0700

    implemented CoppeliaSimulator class and main function

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyrep import PyRep
+import time
+import os
+
+class CoppeliaSimulator():
+    def __init__(self, scene: str) -> None:
+        self._pyrep = PyRep()
+        self._pyrep.launch(os.getcwd() + "/" + scene, headless=False)
+        time.sleep(2.0)
+
+    def start(self) -> None:
+        self._pyrep.start()
+
+    def shutdown(self) -> None:
+        self._pyrep.shutdown()
+
+    def getPyRep(self) -> PyRep:
+        return self._pyrep
+        
+if __name__ == '__main__':
+    cpSim = CoppeliaSimulator("main-scene.ttt")
+    cpSim.start()
+
+    cpSim.shutdown()
