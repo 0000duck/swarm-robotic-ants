e86b924a6197fa6d2e920ddb4a8ecb82c063f0f0
jwande18 <jwande18@asu.edu>
2021-04-27 03:22:29 -0700

    released v0.0.1

--- main.py
+++ main.py
@@ -22,31 +22,51 @@ class CoppeliaSimulator():
 
     def step(self) -> None:
         self._pyrep.step()
-        
+
 if __name__ == '__main__':
     cpsim = CoppeliaSimulator('main-scene.ttt')
     cpsim.start()
 
     units = []
-    for i in range(1, 11):
+    for i in range(1, 6):
         units.append(Unit(cpsim.getPyRep(), i))
+
+    targets0 = [
+        np.array([-10, 10]),
+        np.array([10, -10]),
+        np.array([-10, 10]),
+        np.array([10, -10]),
+        np.array([-10, 10]),
+        np.array([10, -10])
+    ]
     
-    targets = [
-        np.array([10.0, 5.0]),
-        np.array([10.0, -5.0]),
-        np.array([-10.0, 10.0]),
-        np.array([-10.0, -10.0]),
-        np.array([0, 0])
+    targets1 = [
+        np.array([10, -10]),
+        np.array([-10, 10]),
+        np.array([10, -10]),
+        np.array([-10, 10]),
+        np.array([10, -10]),
+        np.array([-10, 10])
     ]
 
-    for target in targets:
-        dist = 1000
+    for t in targets0:
+        units[0].addTarget(t)
+        units[1].addTarget(t)
+
+    for t in targets1:
+        units[2].addTarget(t)
+        units[3].addTarget(t)
+        units[4].addTarget(t)
+
+    while units[0]._targets or units[1]._targets or units[2]._targets or units[3]._targets or units[4]._targets:
         for unit in units:
-            dist = min(dist, unit.seek(target))
+            unit.seek('arrival')
+            unit.separate(units)
+            
+            dist = unit.distTo(unit.getCurrTarget())
+            if dist < 0.5:
+                unit.nextTarget()
+            
+        cpsim.step()
 
-        while dist > 0.5:
-            cpsim.step()
-            for unit in units:
-                dist = min(dist, unit.seek(target))
-
-    cpSim.shutdown()
+    cpsim.shutdown()
--- unit.py
+++ unit.py
@@ -7,9 +7,19 @@ class Unit():
         self._pyrep = pyrep
         self._index = index
 
+        self._targets = []
+
         # unit traits
         self.max_speed = 1.0
         self.max_force = 5.0
+
+        self.min_sep_dist = 2.0
+        self.max_sep_speed = 10.0
+        self.max_sep_force = 5.0
+
+        self.min_follow_dist = 2.0
+
+        self.arrival_radius = 1.5
 
     def getLocation(self):
         ints, floats, strings, byte = self._pyrep.script_call(
@@ -45,13 +55,69 @@ class Unit():
             bytes=''
         )
 
-    def seek(self, target) -> float:
-        position = self.getLocation()
-        desired  = np.subtract(target, position)
-        desired = (desired / la.norm(desired)) * self.max_speed
+    def seek(self, behavior=None):
+        if behavior == 'arrival':
+            radius = self.arrival_radius
+            dist = self.distTo(self.getCurrTarget())
 
-        steer = np.subtract(desired, self.getVelocity())
-        steer = np.clip(steer, None, self.max_force)
+            rated_speed = self.max_speed
+            
+            if dist < radius:
+                rated_speed = self.max_speed * (dist / radius)
+                rated_speed = min(rated_speed, self.max_speed)
 
-        self.applyForce(steer)
-        return la.norm(target - position)
+            position = self.getLocation()
+            desired  = np.subtract(self._targets[0], position)
+            desired = (desired / la.norm(desired)) * rated_speed
+
+            steer = np.subtract(desired, self.getVelocity())
+            steer = np.clip(steer, None, self.max_force)
+
+            self.applyForce(steer)
+        else:
+            position = self.getLocation()
+            desired  = np.subtract(self._targets[0], position)
+            desired = (desired / la.norm(desired)) * self.max_speed
+
+            steer = np.subtract(desired, self.getVelocity())
+            steer = np.clip(steer, None, self.max_force)
+
+            self.applyForce(steer)
+
+    def separate(self, units):
+        steer = np.array([np.nan, np.nan])
+        
+        for unit in units:
+            curr_pos = self.getLocation()
+            unit_pos = unit.getLocation()
+
+            dist = la.norm(curr_pos - unit_pos)
+            if dist < self.min_sep_dist:
+                rep = np.subtract(curr_pos, unit_pos)
+                rep = (rep / la.norm(rep)) * (1 / self.max_sep_speed)
+
+                if np.isnan(steer).any():
+                    steer = rep
+                else:
+                    steer = steer + rep
+
+        if not np.isnan(steer).any():
+            steer = np.clip(steer, None, self.max_sep_force)
+            self.applyForce(steer)
+
+    def addTarget(self, target) -> None:
+        print('#{}: ADDED TARGET: {}'.format(self._index, target))
+        self._targets.append(target)
+            
+    def nextTarget(self) -> None:
+        self._targets.pop(0)
+        print('#{}: NEXT TARGET: {}'.format(self._index, self._targets[0]))
+
+    def getCurrTarget(self):
+        return self._targets[0]
+    
+    # helper function(s)
+    def distTo(self, target) -> float:
+        return la.norm(target - self.getLocation())
+
+    
